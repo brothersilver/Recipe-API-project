b9c26ea8af47d732511666d7aaf6382dd5067355
Jukka Veijanen <jukka.veijanen@solita.fi>
2023-01-01 23:02:03 +0200

    Fix lint errors

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -24,7 +24,8 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check: unittest.mock.MagicMock):
+    def test_wait_for_db_delay(self, patched_sleep,
+                               patched_check: unittest.mock.MagicMock):
         """Test waiting for database when getting OperationalError"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
                                     [OperationalError] * 3 + [True]
